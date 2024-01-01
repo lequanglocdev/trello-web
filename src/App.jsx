1ee7949068279dd665a94760fac5482fbda1db3a
lequanglocdev <quanglocdev@gmail.com>
2024-01-01 23:27:08 +0700

    create Project

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import Button from "@mui/material/Button";
+import AccessAlarmIcon from "@mui/icons-material/AccessAlarm";
+import ThreeDRotation from "@mui/icons-material/ThreeDRotation";
+import HomeIcon from "@mui/icons-material/Home"
+import { pink } from '@mui/material/colors';
+function App() {
+  return (
+    <>
+      <div>Quang loc dev</div>
+      <Button variant="contained">Hello world</Button>
+      <AccessAlarmIcon />
+      <ThreeDRotation />
+      <br />
+      <HomeIcon />
+      <HomeIcon color="primary" />
+      <HomeIcon color="secondary" />
+      <HomeIcon color="success" />
+      <HomeIcon color="action" />
+      <HomeIcon color="disabled" />
+      <HomeIcon sx={{ color: pink[500] }} />
+    </>
+  );
+}
+
+export default App;
